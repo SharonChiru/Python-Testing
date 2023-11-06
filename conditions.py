a408f75318e2a1a6ac5fece462f4f9322e699343
sharonchiru <sharonchiru25@gmail.com>
2023-11-06 15:56:31 +0300

    python testing file added

---
+++ conditions.py
@@ -0,0 +1,75 @@
+# if condition
+relationship= True
+if relationship:
+    print('Taken')
+    #if else statement
+a=300
+b=30
+if a>30:
+    print(False)
+else:
+    print(True)
+
+    a=10
+if a<0:
+    print('Positive number')
+else:
+    print('Negative number')
+# if the condition evaluates to True,
+#the code inside if is executed
+#the code inside else is skipped
+
+#If the condition evaluates to False,
+#the code inside else is executed
+#the code inside if is skipped
+
+#if elif....else statement
+#when everythin is false ,else will  be exceuted
+marks = 90
+if marks>=90:
+    print('grade A')
+elif marks<=80:
+    print("grade B")
+else:
+    print("grade C")
+
+score=50
+if  score>= 50:
+    print('you have done well')
+    print("congartulation")
+marks=35
+if marks <=50:
+    print('Grade A')
+else:
+    print('fail')
+#create a program to check whether a number is positive or negative. for this create a varible named
+#number and assign a float value to it based on the user input. Then using if statement check if the
+# number variable is positive or negative or zero
+#(a)if number is positive , print "the number is positive"
+#(b)if the number is negative ,print"the number is negative"
+#(c)if the number is 0 ,print"the number is 0"
+number=9.5
+if number is 9.5:
+    print('the number is positive')
+elif number is -9.5:
+    print('the number is negative')
+else:
+    print('the number is 0')
+
+number=-9.5
+if number is 9.5:
+    print('the number is positive')
+elif number is -9.5:
+    print('the number is negative')
+else:
+    print('the number is 0')
+
+    number=-5.8
+if number is 9.5:
+    print('the number is positive')
+elif number is -9.5:
+    print('the number is negative')
+else:
+    print('the number is 0')
+
+
---
+++ first code.py
@@ -0,0 +1,49 @@
+site_name = "power learn project"
+
+print (site_name)
+site_name="power learn project"
+print(site_name)
+
+site_name="apple.com"
+print(site_name)
+
+a,b,c=5,4.5,"hello"
+print(a)
+print(b)
+print(c)
+site1=site2="power learn project"
+print(site1)
+print(site2)
+my_name='sharon'
+print(my_name)
+a=10
+b=20
+c=190
+print(a,b,c)
+a=10*2
+b=10*3
+print(a*b)
+A="5+5"
+print(A)
+num1=5
+print(num1,"is type of",type(num1))
+num2=2.75
+print(num2,"is type of",type(num2))
+num3=5j
+print(num3,"is type of",type(num3))
+languages=('python','futter','javasript')
+print(languages[0])
+print(languages[2])
+print(languages[1])
+product = ('microsoft','Xbox ',499.99)
+print(product[2])
+product = ('windows','Xbox ',499.99)
+print(product)
+student_id={112,113,115,114}
+print(student_id)
+print(type(student_id))
+Capital_city={'kenya':'Nairobi','Italy':'rome','Uganda':'Kampala'}
+print(Capital_city)
+print(Capital_city['kenya'])
+print(Capital_city['Nairobi'])
+
