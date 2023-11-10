57bda4461c3e20ce56fd0d310243b1258cfe7294
sharonchiru <sharonchiru25@gmail.com>
2023-11-10 15:24:14 +0300

    printing output and input in python

---
+++ input.py
@@ -0,0 +1,3 @@
+num = input (10)
+print(10,num)
+print(int,type(num))
---
+++ output.py
@@ -0,0 +1,20 @@
+#print statement
+print("Good morning")
+print ('it is rainy today')
+#print with end parameter
+print("Good morning", end= ' ' )
+print('how are you doing')
+#python print with sep parameter
+print('friday', 2023 ,'a sunny day it is', sep=' .')
+#python variables and literals
+number = 98
+name = 'sharon chiru'
+print (5)
+print(number)
+print (name)
+#print concatenated strings
+print('it is a feel good friday ' + ' it must be nice ' + ' feeling freshi barida ')
+#output formating
+a = 5
+b = 0.116
+print('the value of a is {} and b is {}'.format(a,b))
